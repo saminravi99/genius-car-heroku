7c884522532beeb566ee3b36ea83c02dcfc2d549
Samin Israr Ravi <saminisrar1@gmail.com>
2022-04-25 22:44:33 +0600

    committed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Genius Server Running");
+  res.send("Genius Server Running With Heroku");
 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pwygg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -58,9 +58,6 @@ const run = async () => {
 };
 run().catch(console.dir);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
